b82b46d1a65ebfb9850a3c10a7e8e94d4f730144
Genesis2327 <abanalesgenesis76@gmail.com>
2024-12-01 19:01:56 +0800

    Create SIGNUP.Designer.cs

---
+++ SIGNUP.Designer.cs
@@ -0,0 +1,259 @@
+namespace FINALS_LAB_ACT_1
+{
+    partial class SIGNUP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SIGNUP));
+            this.lblWelcomeBack = new System.Windows.Forms.Label();
+            this.lblLoginHere = new System.Windows.Forms.Label();
+            this.lblAlreadyHaveAnAccount = new System.Windows.Forms.Label();
+            this.cbShowPassword = new System.Windows.Forms.CheckBox();
+            this.btnSignUp = new System.Windows.Forms.Button();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.lvlGetStarted = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblSalesInventory = new System.Windows.Forms.Label();
+            this.tbConfirmPassword = new System.Windows.Forms.TextBox();
+            this.lblConfirmPassword = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblWelcomeBack
+            // 
+            this.lblWelcomeBack.AutoSize = true;
+            this.lblWelcomeBack.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcomeBack.Location = new System.Drawing.Point(280, 218);
+            this.lblWelcomeBack.Name = "lblWelcomeBack";
+            this.lblWelcomeBack.Size = new System.Drawing.Size(0, 23);
+            this.lblWelcomeBack.TabIndex = 2;
+            // 
+            // lblLoginHere
+            // 
+            this.lblLoginHere.AutoSize = true;
+            this.lblLoginHere.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoginHere.Location = new System.Drawing.Point(579, 409);
+            this.lblLoginHere.Name = "lblLoginHere";
+            this.lblLoginHere.Size = new System.Drawing.Size(101, 21);
+            this.lblLoginHere.TabIndex = 19;
+            this.lblLoginHere.Text = "Login Here!";
+            this.lblLoginHere.Click += new System.EventHandler(this.lblLoginHere_Click);
+            // 
+            // lblAlreadyHaveAnAccount
+            // 
+            this.lblAlreadyHaveAnAccount.AutoSize = true;
+            this.lblAlreadyHaveAnAccount.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAlreadyHaveAnAccount.Location = new System.Drawing.Point(307, 409);
+            this.lblAlreadyHaveAnAccount.Name = "lblAlreadyHaveAnAccount";
+            this.lblAlreadyHaveAnAccount.Size = new System.Drawing.Size(241, 21);
+            this.lblAlreadyHaveAnAccount.TabIndex = 18;
+            this.lblAlreadyHaveAnAccount.Text = "Already have an account?!";
+            // 
+            // cbShowPassword
+            // 
+            this.cbShowPassword.AutoSize = true;
+            this.cbShowPassword.BackColor = System.Drawing.Color.White;
+            this.cbShowPassword.ForeColor = System.Drawing.Color.Gray;
+            this.cbShowPassword.Location = new System.Drawing.Point(589, 336);
+            this.cbShowPassword.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cbShowPassword.Name = "cbShowPassword";
+            this.cbShowPassword.Size = new System.Drawing.Size(125, 20);
+            this.cbShowPassword.TabIndex = 17;
+            this.cbShowPassword.Text = "Show Password";
+            this.cbShowPassword.UseVisualStyleBackColor = false;
+            this.cbShowPassword.CheckedChanged += new System.EventHandler(this.cbShowPassword_CheckedChanged);
+            // 
+            // btnSignUp
+            // 
+            this.btnSignUp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnSignUp.ForeColor = System.Drawing.Color.White;
+            this.btnSignUp.Location = new System.Drawing.Point(311, 336);
+            this.btnSignUp.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSignUp.Name = "btnSignUp";
+            this.btnSignUp.Size = new System.Drawing.Size(119, 54);
+            this.btnSignUp.TabIndex = 16;
+            this.btnSignUp.Text = "SIGNUP!";
+            this.btnSignUp.UseVisualStyleBackColor = false;
+            this.btnSignUp.Click += new System.EventHandler(this.btnSignUp_Click);
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(311, 181);
+            this.tbPassword.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbPassword.Multiline = true;
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.Size = new System.Drawing.Size(413, 34);
+            this.tbPassword.TabIndex = 15;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(307, 144);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(93, 21);
+            this.lblPassword.TabIndex = 14;
+            this.lblPassword.Text = "Password:";
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Location = new System.Drawing.Point(311, 87);
+            this.tbUsername.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbUsername.Multiline = true;
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(413, 34);
+            this.tbUsername.TabIndex = 13;
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserName.Location = new System.Drawing.Point(307, 49);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(98, 21);
+            this.lblUserName.TabIndex = 12;
+            this.lblUserName.Text = "Username:";
+            // 
+            // lvlGetStarted
+            // 
+            this.lvlGetStarted.AutoSize = true;
+            this.lvlGetStarted.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvlGetStarted.Location = new System.Drawing.Point(288, 12);
+            this.lvlGetStarted.Name = "lvlGetStarted";
+            this.lvlGetStarted.Size = new System.Drawing.Size(125, 23);
+            this.lvlGetStarted.TabIndex = 11;
+            this.lvlGetStarted.Text = "Get Started!";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.lblSalesInventory);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(283, 459);
+            this.panel1.TabIndex = 10;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(71, 115);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(148, 122);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblSalesInventory
+            // 
+            this.lblSalesInventory.AutoSize = true;
+            this.lblSalesInventory.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.lblSalesInventory.Font = new System.Drawing.Font("Impact", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSalesInventory.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.lblSalesInventory.Location = new System.Drawing.Point(28, 239);
+            this.lblSalesInventory.Name = "lblSalesInventory";
+            this.lblSalesInventory.Size = new System.Drawing.Size(229, 41);
+            this.lblSalesInventory.TabIndex = 10;
+            this.lblSalesInventory.Text = "Sales Inventory";
+            // 
+            // tbConfirmPassword
+            // 
+            this.tbConfirmPassword.Location = new System.Drawing.Point(312, 276);
+            this.tbConfirmPassword.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbConfirmPassword.Multiline = true;
+            this.tbConfirmPassword.Name = "tbConfirmPassword";
+            this.tbConfirmPassword.PasswordChar = '*';
+            this.tbConfirmPassword.Size = new System.Drawing.Size(413, 34);
+            this.tbConfirmPassword.TabIndex = 21;
+            // 
+            // lblConfirmPassword
+            // 
+            this.lblConfirmPassword.AutoSize = true;
+            this.lblConfirmPassword.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConfirmPassword.Location = new System.Drawing.Point(308, 239);
+            this.lblConfirmPassword.Name = "lblConfirmPassword";
+            this.lblConfirmPassword.Size = new System.Drawing.Size(161, 21);
+            this.lblConfirmPassword.TabIndex = 20;
+            this.lblConfirmPassword.Text = "Confirm Password:";
+            // 
+            // SIGNUP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 450);
+            this.Controls.Add(this.tbConfirmPassword);
+            this.Controls.Add(this.lblConfirmPassword);
+            this.Controls.Add(this.lblLoginHere);
+            this.Controls.Add(this.lblAlreadyHaveAnAccount);
+            this.Controls.Add(this.cbShowPassword);
+            this.Controls.Add(this.btnSignUp);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.tbUsername);
+            this.Controls.Add(this.lblUserName);
+            this.Controls.Add(this.lvlGetStarted);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblWelcomeBack);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "SIGNUP";
+            this.Text = "SIGNUP";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblWelcomeBack;
+        private System.Windows.Forms.Label lblLoginHere;
+        private System.Windows.Forms.Label lblAlreadyHaveAnAccount;
+        private System.Windows.Forms.CheckBox cbShowPassword;
+        private System.Windows.Forms.Button btnSignUp;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.Label lvlGetStarted;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblSalesInventory;
+        private System.Windows.Forms.TextBox tbConfirmPassword;
+        private System.Windows.Forms.Label lblConfirmPassword;
+    }
+}
