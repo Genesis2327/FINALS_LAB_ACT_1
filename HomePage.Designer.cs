98005c7dae288491b8b2bdda1fb0a861083f6338
Genesis2327 <abanalesgenesis76@gmail.com>
2024-12-01 19:03:18 +0800

    Create HomePage.Designer.cs

---
+++ HomePage.Designer.cs
@@ -0,0 +1,267 @@
+namespace FINALS_LAB_ACT_1
+{
+    partial class HomePage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomePage));
+            this.lblWelcomeBack = new System.Windows.Forms.Label();
+            this.btnMaintenance = new System.Windows.Forms.Button();
+            this.lblDashBoard = new System.Windows.Forms.Label();
+            this.btnDelivery = new System.Windows.Forms.Button();
+            this.btnInventoryMonitoring = new System.Windows.Forms.Button();
+            this.btnPointOfSales = new System.Windows.Forms.Button();
+            this.lblSalesInventory = new System.Windows.Forms.Label();
+            this.btnSalesMonitoring = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnLogOut = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel7.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblWelcomeBack
+            // 
+            this.lblWelcomeBack.AutoSize = true;
+            this.lblWelcomeBack.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcomeBack.Location = new System.Drawing.Point(318, 214);
+            this.lblWelcomeBack.Name = "lblWelcomeBack";
+            this.lblWelcomeBack.Size = new System.Drawing.Size(0, 23);
+            this.lblWelcomeBack.TabIndex = 22;
+            // 
+            // btnMaintenance
+            // 
+            this.btnMaintenance.AutoSize = true;
+            this.btnMaintenance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnMaintenance.Font = new System.Drawing.Font("Century Gothic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMaintenance.ForeColor = System.Drawing.Color.Black;
+            this.btnMaintenance.Location = new System.Drawing.Point(36, 89);
+            this.btnMaintenance.Name = "btnMaintenance";
+            this.btnMaintenance.Size = new System.Drawing.Size(236, 47);
+            this.btnMaintenance.TabIndex = 0;
+            this.btnMaintenance.Text = "Maintenance";
+            this.btnMaintenance.UseVisualStyleBackColor = false;
+            this.btnMaintenance.Click += new System.EventHandler(this.btnMaintenance_Click);
+            // 
+            // lblDashBoard
+            // 
+            this.lblDashBoard.AutoSize = true;
+            this.lblDashBoard.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.lblDashBoard.Font = new System.Drawing.Font("Impact", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDashBoard.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.lblDashBoard.Location = new System.Drawing.Point(73, 11);
+            this.lblDashBoard.Name = "lblDashBoard";
+            this.lblDashBoard.Size = new System.Drawing.Size(158, 37);
+            this.lblDashBoard.TabIndex = 24;
+            this.lblDashBoard.Text = "Dash Board";
+            // 
+            // btnDelivery
+            // 
+            this.btnDelivery.AutoSize = true;
+            this.btnDelivery.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnDelivery.Font = new System.Drawing.Font("Century Gothic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelivery.ForeColor = System.Drawing.Color.Black;
+            this.btnDelivery.Location = new System.Drawing.Point(36, 158);
+            this.btnDelivery.Name = "btnDelivery";
+            this.btnDelivery.Size = new System.Drawing.Size(236, 47);
+            this.btnDelivery.TabIndex = 25;
+            this.btnDelivery.Text = "Delivery";
+            this.btnDelivery.UseVisualStyleBackColor = false;
+            this.btnDelivery.Click += new System.EventHandler(this.btnDelivery_Click);
+            // 
+            // btnInventoryMonitoring
+            // 
+            this.btnInventoryMonitoring.AutoSize = true;
+            this.btnInventoryMonitoring.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnInventoryMonitoring.Font = new System.Drawing.Font("Century Gothic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInventoryMonitoring.ForeColor = System.Drawing.Color.Black;
+            this.btnInventoryMonitoring.Location = new System.Drawing.Point(36, 239);
+            this.btnInventoryMonitoring.Name = "btnInventoryMonitoring";
+            this.btnInventoryMonitoring.Size = new System.Drawing.Size(236, 47);
+            this.btnInventoryMonitoring.TabIndex = 26;
+            this.btnInventoryMonitoring.Text = "Inventory Monitoring";
+            this.btnInventoryMonitoring.UseVisualStyleBackColor = false;
+            this.btnInventoryMonitoring.Click += new System.EventHandler(this.btnInventoryMonitoring_Click);
+            // 
+            // btnPointOfSales
+            // 
+            this.btnPointOfSales.AutoSize = true;
+            this.btnPointOfSales.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnPointOfSales.Font = new System.Drawing.Font("Century Gothic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPointOfSales.ForeColor = System.Drawing.Color.Black;
+            this.btnPointOfSales.Location = new System.Drawing.Point(36, 315);
+            this.btnPointOfSales.Name = "btnPointOfSales";
+            this.btnPointOfSales.Size = new System.Drawing.Size(236, 47);
+            this.btnPointOfSales.TabIndex = 27;
+            this.btnPointOfSales.Text = "Point Of Sales";
+            this.btnPointOfSales.UseVisualStyleBackColor = false;
+            this.btnPointOfSales.Click += new System.EventHandler(this.btnPointOfSales_Click);
+            // 
+            // lblSalesInventory
+            // 
+            this.lblSalesInventory.AutoSize = true;
+            this.lblSalesInventory.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.lblSalesInventory.Font = new System.Drawing.Font("Impact", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSalesInventory.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.lblSalesInventory.Location = new System.Drawing.Point(433, 11);
+            this.lblSalesInventory.Name = "lblSalesInventory";
+            this.lblSalesInventory.Size = new System.Drawing.Size(229, 41);
+            this.lblSalesInventory.TabIndex = 10;
+            this.lblSalesInventory.Text = "Sales Inventory";
+            // 
+            // btnSalesMonitoring
+            // 
+            this.btnSalesMonitoring.AutoSize = true;
+            this.btnSalesMonitoring.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnSalesMonitoring.Font = new System.Drawing.Font("Century Gothic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalesMonitoring.ForeColor = System.Drawing.Color.Black;
+            this.btnSalesMonitoring.Location = new System.Drawing.Point(36, 394);
+            this.btnSalesMonitoring.Name = "btnSalesMonitoring";
+            this.btnSalesMonitoring.Size = new System.Drawing.Size(236, 47);
+            this.btnSalesMonitoring.TabIndex = 28;
+            this.btnSalesMonitoring.Text = "Sales Monitoring";
+            this.btnSalesMonitoring.UseVisualStyleBackColor = false;
+            this.btnSalesMonitoring.Click += new System.EventHandler(this.btnSalesMonitoring_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(708, 11);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(62, 62);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.panel7.Controls.Add(this.btnSalesMonitoring);
+            this.panel7.Controls.Add(this.btnPointOfSales);
+            this.panel7.Controls.Add(this.btnInventoryMonitoring);
+            this.panel7.Controls.Add(this.btnDelivery);
+            this.panel7.Controls.Add(this.lblDashBoard);
+            this.panel7.Controls.Add(this.btnMaintenance);
+            this.panel7.Location = new System.Drawing.Point(1, 63);
+            this.panel7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(311, 493);
+            this.panel7.TabIndex = 23;
+            this.panel7.Paint += new System.Windows.Forms.PaintEventHandler(this.panel7_Paint);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(11, 11);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(63, 41);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 24;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.panel1.Controls.Add(this.btnLogOut);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.lblSalesInventory);
+            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1087, 66);
+            this.panel1.TabIndex = 25;
+            // 
+            // btnLogOut
+            // 
+            this.btnLogOut.AutoSize = true;
+            this.btnLogOut.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnLogOut.Font = new System.Drawing.Font("Century Gothic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogOut.ForeColor = System.Drawing.Color.Black;
+            this.btnLogOut.Location = new System.Drawing.Point(919, 11);
+            this.btnLogOut.Name = "btnLogOut";
+            this.btnLogOut.Size = new System.Drawing.Size(150, 41);
+            this.btnLogOut.TabIndex = 29;
+            this.btnLogOut.Text = "Log Out?";
+            this.btnLogOut.UseVisualStyleBackColor = false;
+            this.btnLogOut.Click += new System.EventHandler(this.btnLogOut_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(312, 66);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(776, 487);
+            this.panel3.TabIndex = 26;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // HomePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1091, 555);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.lblWelcomeBack);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "HomePage";
+            this.Text = "HomePage";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblWelcomeBack;
+        private System.Windows.Forms.Button btnMaintenance;
+        private System.Windows.Forms.Label lblDashBoard;
+        private System.Windows.Forms.Button btnDelivery;
+        private System.Windows.Forms.Button btnInventoryMonitoring;
+        private System.Windows.Forms.Button btnPointOfSales;
+        private System.Windows.Forms.Label lblSalesInventory;
+        private System.Windows.Forms.Button btnSalesMonitoring;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnLogOut;
+    }
+}
